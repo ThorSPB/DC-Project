f95d32acaae33a256fdeba4426391f399ace7c3e
Casperu20 <nicubunea20@gmail.com>
2025-06-02 19:10:11 +0300

    Assignment 12: virtual memor write and read TEST on more ram

--- Assigment_12/src/testbench/TestVirtualMemory.java
+++ Assigment_12/src/testbench/TestVirtualMemory.java
@@ -6,10 +6,23 @@ public class TestVirtualMemory {
     public static void main(String[] args) {
         VirtualMemoryBenchmark bench = new VirtualMemoryBenchmark();
 
-        long fileSize = 2L * 1024 * 1024 * 1024; // 2 GB
+        long fileSize = 10L * 1024 * 1024 * 1024; // 2 GB
         int bufferSize = 1024 * 1024; // 1 MB
 
         bench.run(new Object[]{fileSize, bufferSize});
-        System.out.println(bench.getResult());
+        System.out.println("2GB test = " + bench.getResult());
+
+        // Test with 8 GB
+        //fileSize = 8L * 1024 * 1024 * 1024; // 8 GB
+        //bench.run(new Object[]{fileSize, bufferSize});
+        // System.out.println("8GB Test: " + bench.getResult());
+
+        // Test with 16 GB ----> TEST ON EBI OR SEBI LAPTOP!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+        // fileSize = 16L * 1024 * 1024 * 1024; // 16 GB
+        // bench.run(new Object[]{fileSize, bufferSize});
+        // System.out.println("16GB Test: " + bench.getResult());
+
+        //  TEST ALSO FOR 24 gb
     }
 }
